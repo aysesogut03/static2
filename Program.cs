c4acf1e715c39df8ee07e5ac0af43fdd80c1b39a
aysesogut03 <aysesogut2003@gmail.com>
2024-12-12 22:21:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            calisan c1 = new calisan();
+            c1.calisanID = 125;
+            c1.isim = "Ayşe";
+            c1.soyisim = "Söğüt";
+            calisan.maas = 56.000;
+
+            Console.WriteLine("çalışan ıd:" + c1.calisanID);
+            Console.WriteLine("isim:" + c1.isim);
+            Console.WriteLine("soyisim:" + c1.soyisim);
+            Console.WriteLine("maaş:" + calisan.maas);
+
+            Console.ReadLine(); 
+    }
+    }
+}
---
+++ calisan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    public class calisan
+    {
+        public int calisanID { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public  static double  maas { get; set; }
+
+    }
+}
